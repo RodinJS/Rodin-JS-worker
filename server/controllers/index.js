1d3294cbde657e0b3f03857235e2425efa69f8cf
Grigør <g@yvn.io>
2016-11-10 11:33:42 +0400

    added exec

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const config = require('../config');
 const request = require('request');
+const exec = require('child_process').exec;
 const git = require("nodegit");
 
 module.exports.get = (req, res, next) => {
@@ -10,15 +11,22 @@ module.exports.get = (req, res, next) => {
 module.exports.post = (req, res, next) => {
 	console.log({
 		headers: req.headers,
-		body: req.body,
-		params: req.params,
-		query: req.query
+		body: req.body
 	});
 
 	if(req.body.secret === config.gogs_secret) {
 		let tag = req.body.commits[0].message;
-		console.log("tag: ", tag);
-		res.status(200).send({secret: true, headers: req.headers, body: req.body, tag: tag});
+		tag = tag.replace(/(?:\r\n|\r|\n)/g, '');
+
+		exec(`cd /var/www/cdn/ && mkdir ${tag} && cd ${tag} && git init && git pull http://git.rodin.space/rodin/Rodin-JS.git`, (error, stdout, stderr) => {
+			if (error) {
+				console.error(`exec error: ${error}`);
+				res.status(400).send({error: error, secret: true, headers: req.headers, body: req.body});
+			}
+			console.log(`stdout: ${stdout}`);
+			console.log(`stderr: ${stderr}`);
+			res.status(200).send({secret: true, headers: req.headers, body: req.body});
+		});
 	} else {
 		res.status(400).send({secret: false, headers: req.headers, body: req.body});
 	}
